13f48189d848ffbf1b107595913c49e0876930e8
Anton Sizykh <toxa-v-s@yandex.ru>
2022-05-30 14:11:04 +0300

    Убрал целые в 3-й задаче. Осталась проблема с округлением местами

--- Homework3/Task3.py
+++ Homework3/Task3.py
@@ -7,41 +7,42 @@
 
 def FloatPartDiff(list):
     def floatpart(list):
-        return round(list[i] - list[i]//1,2)
-    min = round(list[0] - list[0]//1,2)
+        return list[i] - list[i]//1
+    min = list[0] - list[0]//1
     max = 0  
     for i in range(len(list)):
-        if floatpart(list) > max:
+        if floatpart(list) > max and type(list) != "<class 'int'>": #отбрасываем целые, как в примере
             max = floatpart(list)
-        if floatpart(list) < min:
+        if floatpart(list) < min and type(list) != "<class 'int'>": #floatpart(list) != 0:
             min = floatpart(list)
     return max-min 
 
 
-# init_list = [1.1, 1.2, 3.1, 5, 10.01] - из примера
-# print(init_list, end=' => ')
-# print(FloatPartDiff(init_list)//0.01/100)
+init_list = [1.1, 1.2, 3.1, 5, 10.01] # из примера
+print(init_list, end=' => ')
+print(FloatPartDiff(init_list)//0.01/100)
 
 
 #Попробуем со своим списком
 from listGen import CreateShortFloatList
 
 my_list = CreateShortFloatList(5,10)
+my_list = [7.83, 0.07, 5.06, 1.28, 0.63]
 print(my_list, end=' => ')
-print(FloatPartDiff(my_list))
+print('%.2f'%FloatPartDiff(my_list))
 
 
-#Возникала проблема с неточностью представления float числа, если округлять
-#с помощью math.floor или просто через математические операции, как я делаю в print 
-a = 0.56-0.08
-print(a)
-print(a//0.01*0.01)
+# #Возникала проблема с неточностью  числа, если округлять
+# #с помощью math. или просто через математические операторы, как я делаю в print 
+# a = 0.56-0.08
+# print(a)
+# print(a//0.01*0.01)
 
-#вот пример, когда запись с умножением не работает, а с делением нормально 
-b = 0.91 - 0.08
-print(b)
-print(b//0.01*0.01)
-print(b//0.01/100)
+# #вот пример, когда запись с умножением не работает, а с делением нормально 
+# b = 0.91 - 0.08
+# print(b)
+# print(b//0.01*0.01)
+# print(b//0.01/100)
 
 #Вопрос к вам - какие надежные методы используют, чтобы обрезать число до нужного знака,
 #и при этом не получить неточности из-за бинарного математического представления float в питоне?
---
+++ Lecture2/code.py
@@ -0,0 +1,37 @@
+# colors = ['red','green','blue']
+# data= open('Lecture2/file.txt', 'w') #a - add, добавляет без перезаписи
+#                             #w - write  - перезапись.
+#                             #r - read , чтение
+# data.writelines(colors) #для списков, цикл записи
+# data.write('This is next line\n')
+# data.close()
+
+# with open('Lecture2/file2.txt', 'w') as data2:  #форма блока кода для работы с файлом. Автоматически закрывает файл по выходу из блока.
+#     data2.write('Header')
+#     data2.writelines(colors)
+
+
+# path = 'Lecture2/file2.txt'
+# data = open(path, 'r')
+# for i in data:#построчная итерация , автоматический перенос строки
+#      print(i)
+# data.close()     
+# exit()
+
+# 1. w+
+#     - Позволяет открывать файл для записи и читать из него.
+#     - Если файла не существует, он будет создан.
+# 2. r+
+#     - Позволяет открывать файл для чтения и дописывать в него.
+#     - Если файла не существует, программа выдаст ошибку.
+
+
+
+def concatenatio(*params): #звездочка, когда произвольное число аргументов
+    res: str = ""
+    for item in params:
+         res += item
+    return res
+print(concatenatio('a', 's', 'd', 'w'))  # asdw
+print(concatenatio('a', '1'))  # a1
+# print(concatenatio(1, 2, 3, 4)) # TypeError: ...
